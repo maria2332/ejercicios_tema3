b63b09fcdd69d47d1b1c74cb140c17784304123b
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-29 16:45:48 +0200

    tests

---
+++ tests.py
@@ -0,0 +1,26 @@
+#test ejercicios
+
+import unittest
+from ejercicios import *
+
+class TestEjercicios(unittest.TestCase):
+    
+        def test_ejercicio1(self):    
+            origen = Torre()
+            destino = Torre()
+            auxiliar = Torre()
+    
+            # Apilando discos en la torre de origen
+            for i in range(6, 0, -1):
+                print("Se ha movido el disco", i)
+                disco = Disco(i)
+                origen.apilar(disco)
+    
+            torres_hanoi(6, origen, destino, auxiliar)
+    
+            self.assertEqual(origen.obtener_altura(), 0)
+            self.assertEqual(destino.obtener_altura(), 6)
+            self.assertEqual(auxiliar.obtener_altura(), 0)
+
+        def test_ejercicio2(self):
+             
