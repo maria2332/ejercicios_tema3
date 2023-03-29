d6afce1a2f5ee0b08473744abf131e7b4f264b32
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-29 16:08:43 +0200

    ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,46 +6,70 @@ y ningún disco podrá ponerse en- cima de otro más pequeño. Se dijo a los sac
 llegaría el fin del mundo. Resolver este problema de la Torre de Hanói.
 """
 
-def torre_hanoi(n, origen, destino, temporal):
+class Disco:
+    def __init__(self, tamaño):
+        self.tamaño = tamaño
+        self.siguiente = None
+
+class Torre:
+    def __init__(self):
+        self.top = None
+        self.altura = 0
+
+    def apilar(self, disco):
+        disco.siguiente = self.top
+        self.top = disco
+        self.altura += 1
+
+    def desapilar(self):
+        if self.top is None:
+            return None
+
+        disco = self.top
+        self.top = disco.siguiente
+        self.altura -= 1
+        return disco
+
+    def obtener_top(self):
+        return self.top
+
+    def obtener_altura(self):
+        return self.altura
+
+    def mover_disco(origen, destino):
+        disco = origen.desapilar()
+        destino.apilar(disco)
+
+
+def torres_hanoi(n, origen, destino, auxiliar):
     if n == 1:
-        print("Mueva el disco 1 de la aguja", origen, "a la aguja", destino)
-        return
-    torre_hanoi(n-1, origen, temporal, destino)
-    print("Mueva el disco", n, "de la aguja", origen, "a la aguja", destino)
-    torre_hanoi(n-1, temporal, destino, origen)
-
-# Ejemplo de uso: mover 3 discos desde la aguja 1 a la aguja 3
-torre_hanoi(3, 1, 3, 2)
+        Torre.mover_disco(origen, destino)
+    else:
+        torres_hanoi(n-1, origen, auxiliar, destino)
+        Torre.mover_disco(origen, destino)
+        torres_hanoi(n-1, auxiliar, destino, origen)
 
 
-# def torre_hanoi(n, origen, destino, temporal):
-#     movimientos = {}  # diccionario para guardar los movimientos calculados
-#     cola = [(n, origen, destino, temporal)]  # cola para iteración por cola
-    
-#     while cola:
-#         n, origen, destino, temporal = cola.pop(0)
-        
-#         if n == 1:
-#             print("Mueva el disco 1 de la aguja", origen, "a la aguja", destino)
-#             movimientos[(n, origen, destino)] = 1
-#         else:
-#             # verificar si el movimiento ya ha sido calculado
-#             if (n-1, origen, temporal) not in movimientos:
-#                 cola.insert(0, (n-1, origen, temporal, destino))
-#                 continue
-#             if (1, origen, destino) not in movimientos:
-#                 cola.insert(0, (1, origen, destino, temporal))
-#                 continue
-#             if (n-1, temporal, destino) not in movimientos:
-#                 cola.insert(0, (n-1, temporal, destino, origen))
-#                 continue
-            
-#             # mover los discos y actualizar el diccionario de movimientos
-#             print("Mueva el disco", n, "de la aguja", origen, "a la aguja", destino)
-#             movimientos[(n, origen, destino)] = 1
-#             movimientos[(n-1, origen, temporal)] = 1
-#             movimientos[(1, origen, destino)] = 1
-#             movimientos[(n-1, temporal, destino)] = 1
 
-# # Ejemplo de uso: mover 3 discos desde la aguja 1 a la aguja 3
-# torre_hanoi(3, 1, 3, 2)
+# Ejemplo de uso:
+origen = Torre()
+destino = Torre()
+auxiliar = Torre()
+
+
+
+# Apilando discos en la torre de origen
+for i in range(6, 0, -1):
+    print("Se ha movido el disco", i)
+    disco = Disco(i)
+    origen.apilar(disco)
+
+
+# Resolviendo la Torre de Hanoi
+torres_hanoi(origen.obtener_altura(), origen, destino, auxiliar)
+
+
+# Imprimiendo el resultado final
+print("Torre de origen: ", origen.obtener_altura())
+print("Torre auxiliar: ", auxiliar.obtener_altura())
+print("Torre de destino: ", destino.obtener_altura())
